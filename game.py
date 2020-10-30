025592878b78894e5b1d659626b81c59f6707858
Andrew Smirnov <smirnov@informatics.ru>
2020-10-30 14:22:16 +0300

    Add type hints to entire game

--- game.py
+++ game.py
@@ -15,62 +15,62 @@ class Game:
     PAUSE_SCENE_INDEX = 3
     current_scene_index = MENU_SCENE_INDEX
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.screen = pygame.display.set_mode(self.SIZE, pygame.RESIZABLE)
         self.scenes = [MenuScene(self), MainScene(self), FinalScene(self), PauseScene(self)]
         self.game_over = False
 
     @staticmethod
-    def exit_button_pressed(event):
+    def exit_button_pressed(event: pygame.event.Event) -> bool:
         return event.type == pygame.QUIT
 
     @staticmethod
-    def exit_hotkey_pressed(event):
+    def exit_hotkey_pressed(event: pygame.event.Event) -> bool:
         return event.type == pygame.KEYDOWN and event.mod & pygame.KMOD_CTRL and event.key == pygame.K_q
 
-    def process_exit_events(self, event):
+    def process_exit_events(self, event: pygame.event.Event) -> None:
         if Game.exit_button_pressed(event) or Game.exit_hotkey_pressed(event):
             self.exit_game()
 
-    def resize_scenes(self):
+    def resize_scenes(self) -> None:
         for scene in self.scenes:
             scene.on_window_resize()
 
-    def process_resize_event(self, event):
+    def process_resize_event(self, event: pygame.event.Event) -> None:
         if event.type != pygame.VIDEORESIZE:
             return
         self.SIZE = self.WIDTH, self.HEIGHT = event.w, event.h
         self.screen = pygame.display.set_mode(self.SIZE, pygame.RESIZABLE)
         self.resize_scenes()
 
-    def process_all_events(self):
+    def process_all_events(self) -> None:
         for event in pygame.event.get():
             self.process_exit_events(event)
             self.process_resize_event(event)
             self.scenes[self.current_scene_index].process_event(event)
 
-    def process_all_logic(self):
+    def process_all_logic(self) -> None:
         self.scenes[self.current_scene_index].process_logic()
 
-    def process_all_draw(self):
+    def process_all_draw(self) -> None:
         self.screen.fill(Color.BLACK)
         self.scenes[self.current_scene_index].process_draw()
         pygame.display.flip()
 
-    def main_loop(self):
+    def main_loop(self) -> None:
         while not self.game_over:
             self.process_all_events()
             self.process_all_logic()
             self.process_all_draw()
             pygame.time.wait(10)
 
-    def set_scene(self, index, resume=False):
+    def set_scene(self, index: int, resume: bool = False) -> None:
         if not resume:
             self.scenes[self.current_scene_index].on_deactivate()
         self.current_scene_index = index
         if not resume:
             self.scenes[self.current_scene_index].on_activate()
 
-    def exit_game(self):
+    def exit_game(self) -> None:
         print('Bye bye')
         self.game_over = True
