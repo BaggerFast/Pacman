03fe89b40293738a07d8db54d6c37e3b42a92fb2
Andrew Smirnov <smirnov@informatics.ru>
2020-10-30 12:41:52 +0300

    Main game class refactor. Split entire code into methods, add exit hotkey

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-import sys
 import pygame
 
 from constants import Color
@@ -8,45 +7,55 @@ from scenes.menu import MenuScene
 
 
 class Game:
+    size = width, height = 800, 600
     MENU_SCENE_INDEX = 0
     MAIN_SCENE_INDEX = 1
     GAMEOVER_SCENE_INDEX = 2
+    current_scene_index = MENU_SCENE_INDEX
 
-    def __init__(self, width=800, height=600):
-        self.width = width
-        self.height = height
-        self.size = self.width, self.height
-
-        self.create_window()
-        self.game_over = False
-        self.wall_collision_count = 0
-        self.ticks = 0
-        self.scenes = [MenuScene(self), MainScene(self), FinalScene(self)]
-        self.current_scene = 0
-
-    def create_window(self):
-        pygame.init()
+    def __init__(self):
         self.screen = pygame.display.set_mode(self.size, pygame.RESIZABLE)
+        self.scenes = [MenuScene(self), MainScene(self), FinalScene(self)]
+        self.game_over = False
+
+    @staticmethod
+    def exit_button_pressed(event):
+        return event.type == pygame.QUIT
+
+    @staticmethod
+    def exit_hotkey_pressed(event):
+        return event.type == pygame.KEYDOWN and event.mod & pygame.KMOD_CTRL and event.key == pygame.K_q
+
+    def process_exit_events(self, event):
+        if Game.exit_button_pressed(event) or Game.exit_hotkey_pressed(event):
+            self.exit_game()
+
+    def process_all_events(self):
+        for event in pygame.event.get():
+            self.process_exit_events(event)
+            self.scenes[self.current_scene_index].process_event(event)
+
+    def process_all_logic(self):
+        self.scenes[self.current_scene_index].process_logic()
+
+    def process_all_draw(self):
+        self.screen.fill(Color.BLACK)
+        self.scenes[self.current_scene_index].process_draw()
+        pygame.display.flip()
 
     def main_loop(self):
         while not self.game_over:
-            eventlist = pygame.event.get()
-            for event in eventlist:
-                if event.type == pygame.QUIT:
-                    print('Пользователь нажал крестик')
-                    self.game_over = True
-                self.scenes[self.current_scene].process_event(event)
-            self.scenes[self.current_scene].process_logic()
-            self.screen.fill(Color.BLACK)
-            self.scenes[self.current_scene].process_draw()
-            pygame.display.flip()
+            self.process_all_events()
+            self.process_all_logic()
+            self.process_all_draw()
             pygame.time.wait(10)
-        sys.exit(0)
 
-    def set_scene(self, index):
-        self.scenes[self.current_scene].on_deactivate()
-        self.current_scene = index
-        self.scenes[self.current_scene].on_activate()
+    def set_scene(self, index, resume=False):
+        if not resume:
+            self.scenes[self.current_scene_index].on_deactivate()
+        self.current_scene_index = index
+        if not resume:
+            self.scenes[self.current_scene_index].on_activate()
 
     def exit_game(self):
         print('Bye bye')
--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import sys
+import pygame
+
+
 from game import Game
 
+
 if __name__ == '__main__':
+    pygame.init()
     g = Game()
     g.main_loop()
+    sys.exit(0)
