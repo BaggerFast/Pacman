f4524e91411b27b6eea393aeeaa9211a9bf64111
Andrew Smirnov <smirnov@informatics.ru>
2020-10-30 16:45:39 +0300

    Run file small refactor

--- run.py
+++ run.py
@@ -1,12 +1,17 @@
+#!/usr/bin/env python3
+
 import sys
 import pygame
-
 
 from game import Game
 
 
-if __name__ == '__main__':
+def main():
     pygame.init()
-    g = Game()
-    g.main_loop()
+    pygame.font.init()
+    Game().main_loop()
     sys.exit(0)
+
+
+if __name__ == '__main__':
+    main()
