1426a21ea54a943ddfc27cd52ebdaa11c3f4d52f
Saveliy Timchenko <watermagle@gmail.com>
2020-11-12 15:13:53 +0300

    Добавлены кнопки в меню паузы

--- scenes/pause.py
+++ scenes/pause.py
@@ -19,10 +19,32 @@ class PauseScene(BaseScene):
 
         # Создание и обработка кнопок
         self.continue_button = Button(
+            self.screen, pygame.Rect(self.screen_width // 2, 200, 180, 60),
+            continue_game, 'CONTINUE', (128, 128, 128),
+            (64, 64, 64), (255, 255, 255), (64, 64, 64),
+            (0, 0, 0), (64, 64, 64)
+        )
+        self.continue_button = Button(
             self.screen, pygame.Rect(
-                self.screen_width, 200, 50, 50
-            ),
-            self.continue_game(), 'CONTINUE', (128, 128, 128),
+                self.screen_width // 2 - self.continue_button.rect.w // 2,
+                150, 180, 60),
+            continue_game, 'CONTINUE', (128, 128, 128),
+            (64, 64, 64), (255, 255, 255), (64, 64, 64),
+            (0, 0, 0), (64, 64, 64)
+        )
+        self.restart_button = Button(
+            self.screen, pygame.Rect(
+                self.screen_width // 2 - self.continue_button.rect.w // 2,
+                250, 180, 60),
+            restart_game, 'RESTART', (128, 128, 128),
+            (64, 64, 64), (255, 255, 255), (64, 64, 64),
+            (0, 0, 0), (64, 64, 64)
+        )
+        self.menu_button = Button(
+            self.screen, pygame.Rect(
+                self.screen_width // 2 - self.continue_button.rect.w // 2,
+                350, 180, 60),
+            main_menu, 'MAIN MENU', (128, 128, 128),
             (64, 64, 64), (255, 255, 255), (64, 64, 64),
             (0, 0, 0), (64, 64, 64)
         )
@@ -31,18 +53,27 @@ class PauseScene(BaseScene):
         self.main_text.draw(self.screen)
         self.continue_button.draw()
         self.continue_button.update()
+        self.restart_button.draw()
+        self.restart_button.update()
+        self.menu_button.draw()
+        self.menu_button.update()
 
-    @staticmethod
-    def continue_game():
-        print('Заглушка CONTINUE')
+    def event_update(self, event):
+        self.continue_button.checkEvents(event)
+        self.restart_button.checkEvents(event)
+        self.menu_button.checkEvents(event)
 
-    @staticmethod
-    def restart_game():
-        print('Заглушка RESTART')
 
-    @staticmethod
-    def main_menu():
-        print('Заглушка MAIN MENU')
+def continue_game():
+    print('Заглушка CONTINUE')
+
+
+def restart_game():
+    print('Заглушка RESTART')
+
+
+def main_menu():
+    print('Заглушка MAIN MENU')
 
 
 # Тест работы меню паузы. Нужен только для разработчиков самого меню
@@ -55,7 +86,7 @@ def test_pause():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            pause.continue_button.checkEvents(event)
+            pause.event_update(event)
         screen.fill(Color.BLACK)
         pause.update_objects()
         pygame.display.flip()
