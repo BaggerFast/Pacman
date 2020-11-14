b5da476f6503d9ec0033b5f0ea6b0432bd65f993
Bogdan Plockiy <bogdan.plockiy@mail.ru>
2020-11-14 14:57:06 +0300

    Class recycling
    - removed the ability to draw.
    - fixed the constructor.

--- lib/BasicObjects/c_lives.py
+++ lib/BasicObjects/c_lives.py
@@ -1,40 +1,28 @@
-import pygame
 from lib.BasicObjects.text import Text
 from py.constants import *
 
 
 class Lives:
-    def __init__(self, screen, coord, size, lives=1, max_lives=5, font="Arial"):
-        self.screen = screen
-        self.font = font
-        self.x = coord[0]
-        self.y = coord[1]
-        self.size = size
+    def __init__(self, lives=1, max_lives=5):
         self.lives = lives
         self.max_lives = max_lives
-        self.text = Text(self.format_live_text(), self.size, [self.x, self.y],
-                         color=Color.WHITE, font=self.font)
-
-    def format_live_text(self):  # Функция форматирования текста для вывода
-        return f'Lives: {self.lives}'
 
     def change_count_lives(self, number):  # Функция для изменеия количества жизней
         if self.max_lives >= self.lives + number >= 0:
             self.lives += number
+        elif self.lives + number >= self.max_lives:
+            self.lives = 5
+        elif self.lives + number <= 0:
+            self.lives = 0
 
-    def process_logic(self):  # Логика класса
-        self.text.update_text(self.format_live_text())
-        self.text.update_position([self.x, self.y])
-
-    def process_draw(self):  # Отрисовка класса
-        self.text.draw(self.screen)
+        print('lives: ', self.lives)
 
 
 def main():
     gameover = False
     size = width, height = 800, 600
     screen = pygame.display.set_mode(size)
-    lives = Lives(screen, [10, 100], 50)  # Создание объекта типа Lives с координатами\
+    lives = Lives()  # Создание объекта типа Lives с координатами\
     # 10,100 и размером текста 50
     while not gameover:
         screen.fill(Color.BLACK)
@@ -47,8 +35,6 @@ def main():
                 elif event.key == pygame.K_a:
                     lives.change_count_lives(-1)
 
-        lives.process_logic()
-        lives.process_draw()
         pygame.display.flip()
 
 
