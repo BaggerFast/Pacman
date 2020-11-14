9556dc71fbee95c50c12258eb80db9933b498469
Daniil Aleksandrov <riosha3@gmail.com>
2020-11-14 19:30:53 +0300

    add pacman class with base class

--- py/classes/objects/character_base.py
+++ py/classes/objects/character_base.py
@@ -1,8 +1,5 @@
 from scenes.base import BaseScene as Scene
 import pygame as pg
-from py.constants import *
-import sys
-import os
 
 
 class Character:
@@ -16,11 +13,11 @@ class Character:
 
     def __init__(self, scene: Scene, image: pg.Surface, start_pos: tuple):
         self.scene = scene
-        self.image = pg.transform.scale(image, (35, 35))
+        self.image = pg.transform.scale(image, (45, 45))
         self.rect = self.image.get_rect()
         self.shift_x = self.shift_y = 0
         self.change_pos(*start_pos)
-        self.speed = 2
+        self.speed = 3
 
     def change_pos(self, x, y):
         self.rect.x = x
@@ -33,49 +30,7 @@ class Character:
     def set_dir(self, dir="none"):
         self.shift_x, self.shift_y = self.direction[dir]
 
-    def check_event(self, event):
-        keys = pg.key.get_pressed()
-        check_sum = keys[pg.K_a] + keys[pg.K_d] + keys[pg.K_s] + keys[pg.K_w]
-        if check_sum > 1 or check_sum < 1:
-            self.set_dir()
-        else:
-            if keys[pg.K_a]:
-                self.set_dir("left")
-            if keys[pg.K_d]:
-                self.set_dir("right")
-            if keys[pg.K_w]:
-                self.set_dir("up")
-            if keys[pg.K_s]:
-                self.set_dir("down")
-
     def draw(self):
         self.scene.screen.blit(self.image, self.rect)
 
 
-def main():
-    pg.init()
-    screen_size = width, height = 800, 600
-    screen = pg.display.set_mode(screen_size, pg.SCALED)
-    exit = True
-    char = Character(Scene(screen),
-                     pg.image.load(os.path.join("images", "pacman.png")),
-                     (10, 10))
-
-    while exit:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                exit = False
-
-            char.check_event(event)
-
-        char.move()
-        screen.fill(Color.BLACK)
-
-        char.draw()
-        pygame.display.flip()
-        pygame.time.wait(10)
-
-
-if __name__ == '__main__':
-    main()
-    sys.exit()
---
+++ py/classes/objects/pacman.py
@@ -0,0 +1,52 @@
+from py.classes.objects.character_base import Character
+from scenes.base import BaseScene as Scene
+import pygame as pg
+from py.constants import *
+import sys
+import os
+
+
+class Pacman(Character):
+    def __init__(self, scene: Scene, image: pg.Surface, start_pos: tuple):
+        super().__init__(scene, image, start_pos)
+
+    def check_event(self, event):
+        keys = pg.key.get_pressed()
+        check_sum = keys[pg.K_a] + keys[pg.K_d] + keys[pg.K_s] + keys[pg.K_w]
+        if check_sum > 1 or check_sum < 1:
+            self.set_dir()
+        else:
+            if keys[pg.K_a]:
+                self.set_dir("left")
+            if keys[pg.K_d]:
+                self.set_dir("right")
+            if keys[pg.K_w]:
+                self.set_dir("up")
+            if keys[pg.K_s]:
+                self.set_dir("down")
+
+def main():
+    pg.init()
+    screen_size = width, height = 800, 600
+    screen = pg.display.set_mode(screen_size, pg.SCALED)
+    exit = True
+    char = Pacman(Scene(screen),
+                     pg.image.load(os.path.join("images", "pacman.png")),
+                     (10, 10))
+    while exit:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit = False
+            char.check_event(event)
+
+        char.move()
+        screen.fill(Color.BLACK)
+
+        char.draw()
+        pg.display.flip()
+        pg.time.wait(10)
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit()
