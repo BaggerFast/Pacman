e71575f6661723059855a696647a1a121940cac7
Daniil Aleksandrov <riosha3@gmail.com>
2020-11-14 16:27:37 +0300

    add init

--- py/classes/objects/character_base.py
+++ py/classes/objects/character_base.py
@@ -1,2 +1,12 @@
+from scenes.base import BaseScene as Scene
+import pygame as pg
+
+
 class Character:
-    pass
+    def __init__(self, image, scene: Scene, start_pos: tuple):
+        self.image = image
+        self.scene = scene
+        self.rect_collision = pg.Rect(0, 0, 50, 50)
+
+    def update_pos(self, x, y):
+        pass
