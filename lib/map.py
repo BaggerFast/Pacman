b168354c3c30d2dd24bc77cbce2a7cf2ad0507c8
PashkovD <dimon2006763@gmail.com>
2020-11-15 11:00:26 +0500

    Ну вот

--- lib/map.py
+++ lib/map.py
@@ -11,7 +11,11 @@ def get_maps_list():
 
 
 def load_map(name="original"):
-    return Map(map_file=name)
+    map = Map(map_file=name)
+    return {"map_name": name, "surface": map.surface,
+            "player_pos": map.player_pos, "ghost_pos": map.ghost_pos,
+            "collision": map.collision, "dots": map.dots,
+            "big_dots": map.big_dots, "fruit_pos": map.fruit_pos}
 
 
 class Map:
@@ -32,37 +36,37 @@ class Map:
         self.__file_close()
 
     def __file_open(self):
-        self.archive = zipfile.ZipFile("maps/" + self.map_name + ".map")
-        temp = self.archive.open("map.json")
-        self.json = json.load(temp)
+        self.__archive = zipfile.ZipFile("maps/" + self.map_name + ".map")
+        temp = self.__archive.open("map.json")
+        self.__json = json.load(temp)
         temp.close()
 
     def __load_tiles(self):
-        self.json["tiles"] = \
-            [pygame.image.load(self.archive.open("tiles/" + i)) for i in
-             self.json["tiles"]]
+        self.__json["tiles"] = \
+            [pygame.image.load(self.__archive.open("tiles/" + i)) for i in
+             self.__json["tiles"]]
 
     def __load_surface(self):
-        for y in range(len(self.json["map"])):
-            for x in range(len(self.json["map"][y])):
-                temp_surface = self.json["tiles"][
-                    self.json["map"][y][x][0]]
-                if len(self.json["map"][y][x]) == 2:
+        for y in range(len(self.__json["map"])):
+            for x in range(len(self.__json["map"][y])):
+                temp_surface = self.__json["tiles"][
+                    self.__json["map"][y][x][0]]
+                if len(self.__json["map"][y][x]) == 2:
                     temp_surface = pygame.transform.flip(
-                        temp_surface, self.json["map"][y][x][1]//4, False)
+                        temp_surface, self.__json["map"][y][x][1] // 4, False)
                     temp_surface = pygame.transform.rotate(
-                        temp_surface, self.json["map"][y][x][1] % 4 * -90)
+                        temp_surface, self.__json["map"][y][x][1] % 4 * -90)
                 self.surface.blit(temp_surface, (x * 8, y * 8))
 
     def __load(self):
-        self.player_pos = self.json["player_pos"]
-        self.ghost_pos = self.json["ghosts_pos"]
-        self.collision = self.json["collision_map"]
+        self.player_pos = self.__json["player_pos"]
+        self.ghost_pos = self.__json["ghosts_pos"]
+        self.collision = self.__json["collision_map"]
         #  колизия 1 - вправо можно, 2 - вниз , 4 - влево, 8 - вверх
         # 9 = 1 + 8 можно вверх и вправо
 
-        self.dots = [[bool(x) for x in y]for y in self.collision]
-        for i in self.json["not_dots_rect"]:
+        self.dots = [[bool(x) for x in y] for y in self.collision]
+        for i in self.__json["not_dots_rect"]:
             for y in range(i[1], i[3] + 1):
                 for x in range(i[0], i[2] + 1):
                     self.dots[y][x] = False
@@ -70,17 +74,19 @@ class Map:
         self.dots[self.player_pos[1]][int(self.player_pos[0] - 0.5)] = False
         self.dots[self.player_pos[1]][int(self.player_pos[0] + 0.5)] = False
         # удаление точек с позиции пакмена
-        self.big_dots = self.json["big_dots_pos"]
+        self.big_dots = self.__json["big_dots_pos"]
 
-        for i in self.big_dots:self.dots[i[1]][i[0]] = False
+        for i in self.big_dots: self.dots[i[1]][i[0]] = False
 
-        self.fruit_pos = self.json["fruit_pos"]
+        self.fruit_pos = self.__json["fruit_pos"]
 
     def __file_close(self):
-        self.archive.close()
+        self.__archive.close()
+        del self.__json["tiles"]
+        del self.__json
 
 
-def map_test():
+'''def map_test():
     print("\nMap list")
     for i in get_maps_list():
         print('  ' + i)
@@ -124,4 +130,4 @@ def map_test():
 
 
 if __name__ == "__main__":
-    map_test()
+    map_test()'''
