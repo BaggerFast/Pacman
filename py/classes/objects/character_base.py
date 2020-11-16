b63d10f0ac227ff538c84802870ae56fd8eed6db
PashkovD <dimon2006763@gmail.com>
2020-11-16 17:57:58 +0500

    Изменен способ отображения

--- py/classes/objects/character_base.py
+++ py/classes/objects/character_base.py
@@ -1,25 +1,28 @@
-from scenes.base import BaseScene as Scene
 import pygame as pg
+
 from lib.BasicObjects.health import Health
+from scenes.base import BaseScene as Scene
 
 
 class Character:
     direction = {
-        "up": (0, -1),
-        "down": (0, 1),
-        "right": (1, 0),
-        "left": (-1, 0),
-        "none": (0, 0)
+        "up": (0, -1, 3),
+        "down": (0, 1, 1),
+        "right": (1, 0, 0),
+        "left": (-1, 0, 2),
+        "none": (0, 0, None)
     }
 
     def __init__(self, scene: Scene, image: pg.Surface, start_pos: tuple):
         self.scene = scene
         self.hp = Health()
-        self.image = pg.transform.scale(image, (45, 45))
+        self.image = image
+        self.draw_image = self.image
         self.rect = self.image.get_rect()
         self.shift_x = self.shift_y = 0
         self.change_pos(*start_pos)
-        self.speed = 3
+        self.speed = 1
+        self.rotate = 0
 
     def change_life(self, points):
         self.hp.change_count_lives(points)
@@ -29,13 +32,25 @@ class Character:
         self.rect.y = y
 
     def move(self):
-        self.rect.x += self.shift_x * self.speed
-        self.rect.y += self.shift_y * self.speed
+        self.rect.x = (self.rect.x + self.shift_x * self.speed
+                       + self.scene.screen.get_width())\
+                      % self.scene.screen.get_width()
+        self.rect.y = (self.rect.y + self.shift_y * self.speed
+                       + self.scene.screen.get_height())\
+                      % self.scene.screen.get_height()
 
     def set_dir(self, dir="none"):
-        self.shift_x, self.shift_y = self.direction[dir]
+        if dir != "none":
+            self.shift_x, self.shift_y, rotate = self.direction[dir]
+            if self.rotate != rotate:
+                self.rotate = rotate
+                self.draw_image = pg.transform.rotate(self.image, -90 * rotate)
+        else:
+            self.draw_image = pg.transform.rotate(self.image, -90 * self.rotate)
 
     def draw(self):
-        self.scene.screen.blit(self.image, self.rect)
-
-
+        self.scene.screen.blit(self.draw_image, self.rect)
+        self.scene.screen.blit(self.draw_image, (self.rect.x+self.scene.screen.get_width(), self.rect.y))
+        self.scene.screen.blit(self.draw_image, (self.rect.x-self.scene.screen.get_width(), self.rect.y))
+        self.scene.screen.blit(self.draw_image, (self.rect.x, self.rect.y+self.scene.screen.get_height()))
+        self.scene.screen.blit(self.draw_image, (self.rect.x, self.rect.y-self.scene.screen.get_height()))
--- py/classes/objects/pacman.py
+++ py/classes/objects/pacman.py
@@ -1,20 +1,24 @@
-from py.classes.objects.character_base import Character
-from scenes.base import BaseScene as Scene
-import pygame as pg
-from py.constants import *
 import sys
-import os
+
+import pygame as pg
+
+from py.classes.objects.character_base import Character
+from py.constants import *
+from scenes.base import BaseScene as Scene
 
 
 class Pacman(Character):
-    def __init__(self, scene: Scene, image: pg.Surface, start_pos: tuple):
+    def __init__(self, scene: Scene, start_pos: tuple):
+        image = pygame.image.load("images/Pacman1.png")
         super().__init__(scene, image, start_pos)
+        self.image_2 = pygame.image.load("images/Pacman2.png")
+        self.animate_timer = pg.time.get_ticks()
 
     def check_event(self, event):
         keys = pg.key.get_pressed()
         check_sum = keys[pg.K_a] + keys[pg.K_d] + keys[pg.K_s] + keys[pg.K_w]
-        if check_sum > 1 or check_sum < 1:
-            self.set_dir()
+        if check_sum == 0:
+            pass
         else:
             if keys[pg.K_a]:
                 self.set_dir("left")
@@ -25,20 +29,25 @@ class Pacman(Character):
             if keys[pg.K_s]:
                 self.set_dir("down")
 
+    def update(self):
+        if pg.time.get_ticks()-self.animate_timer > 500:
+            self.animate_timer = pg.time.get_ticks()
+            self.image, self.image_2 = self.image_2, self.image
+            self.set_dir()
+
 
 def main():
     pg.init()
-    screen_size = width, height = 800, 600
+    screen_size = width, height = 224, 248
     screen = pg.display.set_mode(screen_size, pg.SCALED)
     exit = True
-    char = Pacman(Scene(screen),
-                     pg.image.load(os.path.join("images", "pacman.png")),
-                     (10, 10))
+    char = Pacman(Scene(screen), (10, 10))
     while exit:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 exit = False
             char.check_event(event)
+        char.update()
 
         char.move()
         screen.fill(Color.BLACK)
