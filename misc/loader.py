9ce67b08ad8e98a038354ee1bae2f1c4e845ff28
Andrew Smirnov <smirnov@informatics.ru>
2020-11-19 15:42:04 +0300

    Integrate oll map code into main scene. Remove archive, leave only json file. Json reformat due to accessibility

--- misc/loader.py
+++ misc/loader.py
@@ -1,15 +1,5 @@
 import json
 import os
-import zipfile
-from tempfile import TemporaryDirectory
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
 
 
 class SeedLoader:
@@ -18,7 +8,7 @@ class SeedLoader:
         self.seeds = []
         self.prepare_seeds()
 
-    def remove_seeds_undex_ghost_area(self):
+    def remove_seeds_under_ghost_area(self):
         for i in self.__json["not_dots_rect"]:
             for y in range(i[1], i[3] + 1):
                 for x in range(i[0], i[2] + 1):
@@ -34,7 +24,7 @@ class SeedLoader:
 
     def prepare_seeds(self):
         self.seeds = [[bool(x) for x in y] for y in self.__json["collision_map"]]
-        self.remove_seeds_undex_ghost_area()
+        self.remove_seeds_under_ghost_area()
         self.remove_seeds_under_pacman()
         self.remove_seeds_under_energizers()
 
@@ -48,23 +38,15 @@ class SeedLoader:
 class LevelLoader:
     def __init__(self, filename="original.map"):
         self.filename = filename
-        self.temporary_dir = TemporaryDirectory(prefix='pacman__')
-        self.__unpack_archive()
         self.__load_map_json()
         self.movements_map = self.get_movements_data()
         self.seed_loader = SeedLoader(self.__json)
         self.seed_data = self.seed_loader.get_seed_data()
         self.energizer_data = self.seed_loader.get_energizer_data()
 
-    def __unpack_archive(self):
-        self.__archive = zipfile.ZipFile(os.path.join("maps", self.filename))
-        self.__archive.extractall(self.temporary_dir.name)
-        self.__archive.close()
-
     def __load_map_json(self):
-        temp = open(os.path.join(self.temporary_dir.name, "map.json"))
-        self.__json = json.load(temp)
-        temp.close()
+        with open(os.path.join('maps', 'map.json')) as f:
+            self.__json = json.load(f)
 
     def get_map_data(self):
         return self.__json['map']
@@ -78,17 +60,11 @@ class LevelLoader:
     def get_energizer_data(self):
         return self.seed_loader.get_energizer_data()
 
-    def get_seeds_data(self):
-        self.seeds = [[bool(x) for x in y] for y in self.movements_map]
-        for i in self.__json["not_dots_rect"]:
-            for y in range(i[1], i[3] + 1):
-                for x in range(i[0], i[2] + 1):
-                    self.seeds[y][x] = False
+    def get_player_position(self):
+        return self.__json["player_pos"]
 
-        # удаление точек с позиции пакмена
-        self.seeds[self.player_position[1]][int(self.player_position[0] - 0.5)] = False
-        self.seeds[self.player_position[1]][int(self.player_position[0] + 0.5)] = False
+    def get_ghost_positions(self):
+        return self.__json["ghosts_pos"]
 
-        self.big_dots = self.__json["big_dots_pos"]
-        for i in self.big_dots:
-            self.seeds[i[1]][i[0]] = False
+    def get_fruit_position(self):
+        return self.__json["fruit_pos"]
--- scenes/main.py
+++ scenes/main.py
@@ -12,6 +12,10 @@ class GameScene(BaseScene):
         self.map_data = self.loader.get_map_data()
         self.seed_data = self.loader.get_seed_data()
         self.energizer_data = self.loader.get_energizer_data()
+        self.movements_data = self.loader.get_movements_data()
+        self.player_position = self.loader.get_player_position()
+        self.ghost_positions = self.loader.get_ghost_positions()
+        self.fruit_position = self.loader.get_fruit_position()
         super().__init__(game)
 
     def create_objects(self) -> None:
@@ -26,3 +30,30 @@ class GameScene(BaseScene):
 
     def start_pause(self):
         self.game.set_scene(self.game.SCENE_PAUSE)
+
+    def additional_draw(self) -> None:
+        # Temporary draw
+        x_shift = 0
+        y_shift = 20
+
+        # pacman
+        pygame.draw.circle(self.screen, (255, 255, 0),
+                           (x_shift + self.player_position[0] * 8 + 4, y_shift + self.player_position[1] * 8 + 4), 8)
+
+        # ghosts
+        pygame.draw.circle(self.screen, (255, 0, 0),
+                           (x_shift + self.ghost_positions[0][0] * 8 + 4, y_shift + self.ghost_positions[0][1] * 8 + 4),
+                           8)
+        pygame.draw.circle(self.screen, (255, 0, 255),
+                           (x_shift + self.ghost_positions[1][0] * 8 + 4, y_shift + self.ghost_positions[1][1] * 8 + 4),
+                           8)
+        pygame.draw.circle(self.screen, (0, 0, 255),
+                           (x_shift + self.ghost_positions[2][0] * 8 + 4, y_shift + self.ghost_positions[2][1] * 8 + 4),
+                           8)
+        pygame.draw.circle(self.screen, (0, 255, 0),
+                           (x_shift + self.ghost_positions[3][0] * 8 + 4, y_shift + self.ghost_positions[3][1] * 8 + 4),
+                           8)
+
+        # fruit
+        pygame.draw.circle(self.screen, (255, 0, 0),
+                           (x_shift + self.fruit_position[0] * 8 + 4, y_shift + self.fruit_position[1] * 8 + 4), 4)
