4d773272e1c127ef121ec09485096fdbee92ed22
Ivan Ignatov <vanya.ignatow2012@yandex.ru>
2020-11-20 18:59:20 +0300

    Added collision to seeds and energizers

--- objects/seed.py
+++ objects/seed.py
@@ -1,7 +1,7 @@
 import pygame
 
 from objects.base import DrawableObject
-
+from misc.constants import CELL_SIZE
 
 def main():
     pass
@@ -17,18 +17,35 @@ class SeedContainer(DrawableObject):
         self.x = x
         self.y = y
         self.seeds = seed_data
-        self.energizer_data = energizer_data
+        self.energizers = energizer_data
 
     def draw_seeds(self):
         for row in range(len(self.seeds)):
             for col in range(len(self.seeds[row])):
                 if self.seeds[row][col]:
-                    pygame.draw.circle(self.game.screen, (255, 255, 255), (self.x + col * 8 + 4, self.y + row * 8 + 4), 1)
+                    pygame.draw.circle(self.game.screen, (255, 255, 255), (self.x + col * CELL_SIZE + CELL_SIZE/2,
+                                                                           self.y + row * CELL_SIZE + CELL_SIZE/2), 1)
 
     def draw_energizers(self):
-        for energizer in self.energizer_data:
-            pygame.draw.circle(self.game.screen, (255, 255, 255), (self.x + energizer[0] * 8 + 4, self.y + energizer[1] * 8 + 4), 4)
+        for energizer in self.energizers:
+            pygame.draw.circle(self.game.screen, (255, 255, 255), (self.x + energizer[0] * CELL_SIZE + CELL_SIZE/2,
+                                                                   self.y + energizer[1] * CELL_SIZE + CELL_SIZE/2), 4)
 
     def process_draw(self):
         self.draw_seeds()
         self.draw_energizers()
+
+    def process_collision(self, object): # for pacman only
+        for seed in self.seeds:
+            for row in range(len(self.seeds)):
+                for col in range(len(self.seeds[row])):
+                    if self.seeds[row][col] and row * CELL_SIZE + 18 == object.rect.y:
+                        if col * CELL_SIZE == object.rect.x:
+                            self.seeds[row][col] = None
+                            return True, "seed"
+        for energizer in self.energizers:
+            if energizer[1] * CELL_SIZE + 18 == object.rect.y:
+                if energizer[0] * CELL_SIZE == object.rect.x:
+                    self.energizers.remove(energizer)
+                    return True, "enrgizer"
+        return False
