9f95079ae20b4d9954db4175613b5739054ab6ba
PashkovD <dimon2006763@gmail.com>
2020-11-20 21:55:31 +0500

    Added animator

--- objects/pacman.py
+++ objects/pacman.py
@@ -1,6 +1,7 @@
 import pygame
 
 from misc.path import get_image_path_for_animator
+from misc.constants import CELL_SIZE
 from objects.character_base import Character
 from misc.animator import Animator
 
@@ -11,6 +12,7 @@ class Pacman(Character):
             get_image_path_for_animator('Pacman', 'walk')
         )
         super().__init__(game, self.animator, start_pos)
+        self.feature_rotate = "none"
 
     def process_event(self, event):
         action = {
@@ -21,9 +23,27 @@ class Pacman(Character):
         }
         if event.type == pygame.KEYDOWN and event.key in action.keys():
             self.go()
-            self.set_direction(action[event.key])
+            self.feature_rotate = action[event.key]
 
-    def process_logic(self):
-        self.animator.timer_check()
+    def process_logic(s):
+        s.animator.timer_check()
+        if s.in_center():
+            if s.move_to(s.rotate):
+                s.go()
+            else:
+                s.stop()
+            c = s.direction[s.feature_rotate][2]
+            if s.move_to(c):
+                s.set_direction(s.feature_rotate)
         super().process_logic()
 
+    def movement_cell(self):
+        scene = self.game.scenes[self.game.current_scene_index]
+        cell = scene.movements_data[(self.rect.y-12) // CELL_SIZE][self.rect.x // CELL_SIZE+1]
+        return "{0:04b}".format(cell)[::-1]
+
+    def move_to(self,direction):
+        return self.movement_cell()[direction] == "1"
+
+    def in_center(self) -> bool:
+        return self.rect.x % CELL_SIZE == 6 and (self.rect.y-20) % CELL_SIZE == 6
