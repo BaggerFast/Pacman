2b3d2f96a465ae1d06177ccc3602c76ebe97d3a9
Bogdan Plockiy <bogdan.plockiy@mail.ru>
2020-11-23 18:27:55 +0300

    Added class GameoverScene

---
+++ scenes/gameover.py
@@ -0,0 +1,10 @@
+import pygame as pg
+from objects.button import ButtonController, Button
+from objects.text import Text
+from scenes.base import BaseScene
+from misc.constants import Color, Font
+from scenes.main import GameScene
+
+
+class GameoverScene(BaseScene):
+    pass
