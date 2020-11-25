975c4bebba638a4dbdefd2851ec1039fcb88018a
Alice Kiseleva <amunetchtli@gmail.com>
2020-11-25 13:18:50 +0300

    Renamed fruits images

--- objects/fruits.py
+++ objects/fruits.py
@@ -6,7 +6,7 @@ class Fruit(DrawableObject):
     def __init__(self, game, screen, x, y):
         super().__init__(game)
         self.screen = screen
-        self.anim = Animator(get_image_path_for_animator('fruit', 'cherry'), 70, False, False)
+        self.anim = Animator(get_image_path_for_animator('fruit'), 70, False, False)
         self.image = self.anim.current_image
         self.current_image = self.anim.current_image_index
         self.rect = self.anim.current_image.get_rect()
