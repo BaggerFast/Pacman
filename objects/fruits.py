56bd032380aed39d9dc28b4829246841a9be1188
Bogdan Plockiy <bogdan.plockiy@mail.ru>
2020-11-30 16:43:48 +0300

    Added functionality
    -Added "get_cur_index" in "animator.py"
    -Added "__score", "__creating_scores"
    -Changed "change_image"

--- objects/fruits.py
+++ objects/fruits.py
@@ -17,14 +17,20 @@ class Fruit(DrawableObject):
         self.rect = self.__anim.current_image.get_rect()
         self.rect.x = x - self.rect.width // 2
         self.rect.y = y - self.rect.height // 2
+        self.__scores = []
+        self.__creating_scores()
         self.__drawing = False
         self.__start_time = None
         self.__eat_timer = 90
-        self.__score_to_eat = randint(100, 500)
+        self.__score_to_eat = 70
 
     def __draw_fruit(self):
         if self.__drawing:
             self.screen.blit(self.__anim.current_image, self.rect)
+
+    def __creating_scores(self):
+        for i in range(self.__anim.get_len_anim()):
+            self.__scores.append(randint(100, 500))
 
     def __check_score(self):
         if self.__check_last_score():
@@ -42,10 +48,12 @@ class Fruit(DrawableObject):
             self.__start_time = None
             self.__score_to_eat = self.game.score.score + self.__eat_timer
             self.__drawing = False
-            self.__change_image()
+            self.change_image()
 
-    def __change_image(self) -> None:
-        self.__anim.change_cur_image(randint(0, self.__anim.get_len_anim() - 1))
+    def change_image(self) -> None:  # __change_image
+        # self.__anim.change_cur_image(randint(0, self.__anim.get_len_anim() - 1))
+        self.__anim.change_cur_image((self.__anim.get_cur_index() + 1) % self.__anim.get_len_anim())
+        print("len", self.__anim.get_len_anim())
 
     def process_collision(self, object) -> Tuple[bool, str]:
         """
@@ -53,16 +61,17 @@ class Fruit(DrawableObject):
         :return: is objects in collision (bool) and self type (str)
         """
         if self.__drawing:
-            if (self.rect.x == min(object.rect.left, object.rect.right))\
+            if (self.rect.x == min(object.rect.left, object.rect.right)) \
                     and (self.rect.y == object.rect.y):
                 self.__drawing = False
-                self.__score_to_eat = self.game.score.score + self.__eat_timer + Points.POINT_PER_FRUIT
-                self.__change_image()
+                self.__score_to_eat = self.game.score.score + self.__eat_timer + \
+                                      self.__scores[self.__anim.get_cur_index()] + Points.POINT_PER_FRUIT
+                self.change_image()
                 return True, "fruit"
         return False, ""
 
     def process_logic(self):
-        self.__check_score() if self.__drawing else self.__check_score()
+        self.__check_time() if self.__drawing else self.__check_score()
 
     def process_draw(self) -> None:
         self.__draw_fruit()
