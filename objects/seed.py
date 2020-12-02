0398c3d516194912f3ed39e9b1f04c61f58de8b9
Viacheslav Leshchenko <slaval.125.sl@gmail.com>
2020-12-02 19:09:08 +0300

    Bugfix

--- objects/seed.py
+++ objects/seed.py
@@ -78,6 +78,6 @@ class SeedContainer(DrawableObject):
                     return True
 
     def is_field_empty(self) -> bool:
-        if self.__seeds_on_field == 200:
+        if self.__seeds_on_field == 0:
             return True
         return False
