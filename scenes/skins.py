11f56fc0bc93fe01c569c0d72bfd8a32843f2508
Eugeny Arkhipov <eugenyarkhipov1109@gmail.com>
2020-12-07 21:11:35 +0300

    add cosmetic changes regard positions

--- scenes/skins.py
+++ scenes/skins.py
@@ -37,16 +37,16 @@ class Scene(base.Scene):
         self.fruit_images = get_list_path('png', 'images', 'fruit')
         self.fruits_for_skins = {
             0: {           #Default
-                0: 0,
-                1: 0,
+                6: 5,      #устанавливаете значение которое нужно для покупки
+                2: 1,      #устанавливаете значение которое нужно для покупки
             },
             1: {           #Chrome
-                3: 3,
-                4: 2,
+                2: 10,      #устанавливаете значение которое нужно для покупки
+                4: 7,      #устанавливаете значение которое нужно для покупки
             },
             2: {           #Half-life
-                6: 4,
-                7: 3,
+                3: 0,      #устанавливаете значение которое нужно для покупки
+                4: 1,      #устанавливаете значение которое нужно для покупки
             }
                 }
         self.__create_title()
@@ -55,9 +55,10 @@ class Scene(base.Scene):
         self.objects = []
         self.preview = copy(self.game.skins.current.image)
         self.objects.append(self.preview)
+        self.create_buttons()
         self.create_fruits_and_text_we_have()
         self.create_fruits_and_text_for_skins()
-        self.create_buttons()
+
 
     def create_fruits_and_text_we_have(self):
         for index in range(len(self.fruit_images)):
@@ -68,14 +69,16 @@ class Scene(base.Scene):
             self.objects.append(text)
 
     def create_fruits_and_text_for_skins(self):
-        index_pos_y = 33
+        index_pos_y = self.button_pos_multiply_y
         index_pos_x = 20
+        pos_regarding_buttons_x = self.button_pos_x + 45
+        pos_regarding_buttons_y = self.button_pos_y - 6
         for index in self.fruits_for_skins:
             multiply_x = 0
             for i in self.fruits_for_skins[index]:
-                fruit = ImageObject(self.game, self.fruit_images[i], (self.game.width // 2 - 20 + index_pos_x * multiply_x, 109 + index_pos_y * index))
+                fruit = ImageObject(self.game, self.fruit_images[i], (pos_regarding_buttons_x + index_pos_x * multiply_x, pos_regarding_buttons_y + index_pos_y * index))
                 text = Text(self.game, str(self.fruits_for_skins[index][i]), 10)
-                text.move_center(self.game.width // 2 - 20 + index_pos_x * multiply_x, 109 + index_pos_y * index)
+                text.move_center(pos_regarding_buttons_x + index_pos_x * multiply_x, pos_regarding_buttons_y + index_pos_y * index)
                 self.objects.append(fruit)
                 self.objects.append(text)
                 multiply_x +=1
@@ -93,14 +96,16 @@ class Scene(base.Scene):
             1: ('CHROME', self.game.skins.chrome),
             2: ('HALF-LIFE', self.game.skins.half_life),
         }
-
+        self.button_pos_x = self.game.width // 2 - 65
+        self.button_pos_y = 115
+        self.button_pos_multiply_y = 33
         for i in range(len(skins)):
             buttons.append(self.SkinButton(
                 game=self.game,
                 geometry=pg.Rect(0, 0, 75, 33),
                 text=skins[i][0],
                 value=skins[i][1],
-                center=(self.game.width // 2 - 65, 115 + i * 33),
+                center=(self.button_pos_x, 115 + i * self.button_pos_multiply_y),
                 text_size=Font.BUTTON_FOR_SKINS_TEXT_SIZE,
                 active=skins[i][1].name in self.game.unlocked_skins))
 
