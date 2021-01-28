2b3130e4bc070ae84e6a919c5bffa8cffcaf8a86
Viacheslav Leshchenko <slaval.125.sl@gmail.com>
2021-01-28 10:50:08 +0300

    Add exception logging

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import sys
+from datetime import datetime
+import traceback
 import pygame as pg
 from game import Game
 
@@ -7,8 +9,16 @@ def main():
     pg.display.init()
     pg.font.init()
     pg.mixer.init()
-    game = Game()
-    game.main_loop()
+    game: Game
+    try:
+        game = Game()
+        game.main_loop()
+    except Exception:
+        print(traceback.format_exc())
+        with open(f"exception-{datetime.now().strftime('%m-%d-%Y-%H-%M-%S')}.log", "w") as file:
+            file.write(traceback.format_exc())
+        game.exit_game()
+
     sys.exit(0)
 
 
