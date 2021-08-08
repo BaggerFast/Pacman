7387cbf972bed4eca32f7e2eb8993ddc44aa1207
Daniil Aleksandrov <riosha3@gmail.com>
2021-08-08 22:41:03 +0300

    пофиксил проблему правого поворота у пакмеена

--- misc/creator_sprites.py
+++ misc/creator_sprites.py
@@ -1,25 +1,15 @@
-from PIL import Image, ImageDraw
+from PIL import Image
 from misc.path import get_path
 
 
 def create():
-    # 14*8, 14
-    # images = []
-    # for name in range(7):
-    #     images.append(Image.open(get_path(f"images/fruit/{name}.png")))
-    # res = 14*7, 14
-    # r = 14
-    new_im = Image.new(mode='RGBA', size=(18, 9))
-    black = Image.new(mode='RGBA', size=(9, 9))
-    draw = ImageDraw.Draw(black)
-    draw.ellipse((0, 0, 8, 8), 'white')
-    white = Image.new(mode='RGBA', size=(9, 9))
-    new_im.paste(black, (0, 0))
-    new_im.paste(white, (10, 0))
-    # for i in range(len(images)):
-    #     temp = Image.new(mode='RGBA', size=(14, 14))
-    #     old_size = images[i].size
-    #     new_size = r, r
-    #     temp.paste(images[i], (round((new_size[0]-old_size[0])/2), round((new_size[1]-old_size[1])/2)))
-    #     new_im.paste(images[i], (i*r, 0))
-    new_im.save(get_path(f'images/big_seed.png'))
+    for name in ['chrome', 'half_life', 'default', 'edge', 'windows', 'pokeball']:
+        image = Image.open(get_path(f"images/pacman/{name}/walk.png"))
+        new_im = image.crop((0, 0, 54, 13))
+        data = Image.new('RGBA', (54, 13))
+        for i in range(4):
+            temp = new_im.crop((i*13, 0, (i+1)*13, 13))
+            temp = temp.transpose(Image.FLIP_LEFT_RIGHT)
+            data.paste(temp, (i*13, 0, (i+1)*13, 13))
+        image.paste(data, (0, 26, 54, 39))
+        image.save(get_path(f'images/pacman/{name}/walk.png'))
