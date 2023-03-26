07ef60725b96ea0b5d92574dd69f92474580a80d
baggerfast <riosha3@gmail.com>
2023-03-26 13:09:32 +0300

    UPDATE: rework levels scene, new ui

--- pacman/scenes/levels.py
+++ pacman/scenes/levels.py
@@ -1,47 +1,14 @@
-from copy import copy
-
 import pygame as pg
 
+from pacman.data_core import KbKeys, Colors
 from pacman.misc import Font
 from pacman.misc.serializers import LevelStorage
-from pacman.objects import ButtonController, Text
-from pacman.objects.button import Button
+from pacman.objects import ButtonController, Text, ImageObject
 from pacman.scenes import base
 
 
 class LevelsScene(base.Scene):
-    class LvlButton(Button):
-        def __init__(self, **args):
-            self.value = args.pop("value")
-            super().__init__(**args)
-
-        def click(self):
-            self.game.sounds.click.play()
-            LevelStorage().current = self.value[0]
-            self.game.records.update_records()
-            self.game.scenes.set(self.game.scenes.MENU, reset=True)
-
-        def select(self) -> None:
-            super().select()
-            self.game.scenes.current.is_current = True
-            self.game.scenes.current.preview.image = self.value[1].image
-
-        def deselect(self) -> None:
-            super().deselect()
-            if not self.game.scenes.current.is_current:
-                self.game.scenes.current.preview.image = self.game.maps.images[LevelStorage().current].image
-
-    __buttons_on_scene = 4
-
-    def process_event(self, event: pg.event.Event) -> None:
-        self.is_current = False
-        super().process_event(event)
-
     def create_static_objects(self):
-        self.is_current = False
-        self.__scroll = LevelStorage().current
-        self.__scroll = min(self.__scroll, self.game.maps.count - self.__buttons_on_scene)
-        self.__scroll = max(self.__scroll, 0)
         self.__create_title()
 
     def __create_title(self) -> None:
@@ -51,56 +18,78 @@ class LevelsScene(base.Scene):
 
     def create_buttons(self) -> None:
         buttons = []
-        for i in range(self.__scroll, self.__scroll + self.__buttons_on_scene):
-            buttons.append(
-                self.LvlButton(
-                    game=self.game,
-                    geometry=pg.Rect(0, 0, 100, 40),
-                    value=(i, self.game.maps.images[i]),
-                    text="LEVEL" + f"{i + 1}",
-                    center=(self.game.width // 2 - 55, (85 + 40 * (i - self.__scroll))),
-                    text_size=Font.BUTTON_TEXT_SIZE - 4,
-                    active=i in LevelStorage().unlocked,
-                )
-            )
         buttons.append(
             self.SceneButton(
                 game=self.game,
-                geometry=pg.Rect(0, 0, 180, 40),
+                rect=pg.Rect(0, 0, 180, 40),
                 text="MENU",
                 scene=(self.game.scenes.MENU, False),
-                center=(self.game.width // 2, 250),
                 text_size=Font.BUTTON_TEXT_SIZE,
             ).move_center(self.game.width // 2, 250)
         )
         self.__button_controller = ButtonController(buttons)
         self.objects.append(self.__button_controller)
 
-    def unlocked(self) -> int:
-        return len(LevelStorage().unlocked)
+    @property
+    def current_level(self) -> int:
+        return LevelStorage().current
 
     def create_objects(self) -> None:
         self.objects = []
-        self.preview = self.game.maps.images[LevelStorage().current]
+        self.preview: ImageObject = self.game.maps.images[LevelStorage().current]
+        self.preview.smoothscale(224 * 0.6, 248 * 0.6)
+        self.preview.move_center(self.game.size[0] // 2, self.game.size[1] // 2)
         self.objects.append(self.preview)
-        self.create_buttons()
 
-    def scroll_threshold(self) -> None:
-        self.__scroll = min(self.__scroll, self.game.maps.count - self.__buttons_on_scene)
-        self.__scroll = max(self.__scroll, 0)
+        self.text = Text(f"Level: {self.current_level+1}/{LevelStorage().level_count}", 20).move_center(
+            self.game.size[0] // 2, self.game.size[1] // 2
+        )
+        self.text2 = Text(f"L", 40, color=Colors.DARK_GRAY).move_center(
+            self.game.size[0] // 6 - 10, self.game.size[1] // 2
+        )
+        self.text3 = Text(f"R", 40, color=Colors.DARK_GRAY).move_center(
+            self.game.size[0] - (self.game.size[0] // 6 - 16), self.game.size[1] // 2
+        )
+        if self.current_level == 0:
+            self.text2.color = Colors.BLACK
+        if self.current_level == LevelStorage().level_count - 1:
+            self.text3.color = Colors.BLACK
+            self.text = Text(f"Level: {self.current_level + 1}", 20).move_center(
+                self.game.size[0] // 2, self.game.size[1] // 2
+            )
+        elif self.current_level + 1 not in LevelStorage().unlocked:
+            self.text3.color = Colors.BLACK
+
+        self.objects.append(self.text)
+        self.objects.append(self.text2)
+        self.objects.append(self.text3)
+        self.create_buttons()
 
     def additional_event_check(self, event: pg.event.Event) -> None:
         if event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
             self.game.scenes.set(self.game.scenes.MENU)
-        elif event.type == pg.MOUSEWHEEL:
-            self.__scroll -= event.y
-            self.scroll_threshold()
-            self.create_objects()
         elif event.type == pg.KEYDOWN:
-            if event.key in (pg.K_e, pg.K_q):
-                if event.key == pg.K_e:
-                    self.__scroll += 1
-                elif event.key == pg.K_q:
-                    self.__scroll -= 1
-                self.scroll_threshold()
-                self.create_objects()
+            if event.key in KbKeys.RIGHT and self.current_level != LevelStorage().level_count - 1:
+                if self.current_level + 1 in LevelStorage().unlocked:
+                    LevelStorage().set_next_level()
+            elif event.key in KbKeys.LEFT and self.current_level != 0:
+                LevelStorage().set_prev_level()
+            self.preview: ImageObject = self.game.maps.images[self.current_level]
+            self.preview.smoothscale(224 * 0.6, 248 * 0.6)
+            self.preview.move_center(self.game.size[0] // 2, self.game.size[1] // 2)
+            self.objects.append(self.preview)
+            self.text3.color = Colors.DARK_GRAY
+            self.text2.color = Colors.DARK_GRAY
+            if self.current_level == 0:
+                self.text2.color = Colors.BLACK
+            if self.current_level == LevelStorage().level_count - 1:
+                self.text3.color = Colors.BLACK
+                self.text = Text(f"Level: {self.current_level + 1}", 20).move_center(
+                    self.game.size[0] // 2, self.game.size[1] // 2
+                )
+            elif self.current_level + 1 not in LevelStorage().unlocked:
+                self.text3.color = Colors.BLACK
+            self.text = Text(f"Level: {self.current_level + 1}/{LevelStorage().level_count}", 20).move_center(
+                self.game.size[0] // 2, self.game.size[1] // 2
+            )
+            self.objects.append(self.text)
